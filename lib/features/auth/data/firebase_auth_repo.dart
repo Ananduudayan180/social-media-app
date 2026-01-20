5b1b25c445a2956de721f3f6777f448f88fce056
Anandu Udayan <anandhuudayan180@gamil.com>
2026-01-20 14:58:46 +0530

    feat: implement firebase authentication methods

--- lib/features/auth/data/firebase_auth_repo.dart
+++ lib/features/auth/data/firebase_auth_repo.dart
@@ -1,32 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:social_media_app/features/auth/domain/entities/app_user.dart';
 import 'package:social_media_app/features/auth/domain/repos/auth_repo.dart';
 
 class FirebaseAuthRepo implements AuthRepo {
-  @override
-  Future<AppUser?> getCurrentUser() {
-    // TODO: implement getCurrentUser
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<AppUser?> logInWithEmailPassword(String email, String password) {
-    // TODO: implement logInWithEmailPassword
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> logOut() {
-    // TODO: implement logOut
-    throw UnimplementedError();
-  }
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
   @override
   Future<AppUser?> registerWithEmailPassword(
     String name,
     String email,
     String password,
-  ) {
-    // TODO: implement registerWithEmailPassword
-    throw UnimplementedError();
+  ) async {
+    try {
+      UserCredential userCredential = await firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      final user = AppUser(
+        uid: userCredential.user!.uid,
+        name: name,
+        email: email,
+      );
+      return user;
+    } catch (e) {
+      throw Exception('Signup failed $e');
+    }
+  }
+
+  @override
+  Future<AppUser?> logInWithEmailPassword(String email, String password) async {
+    try {
+      UserCredential userCredential = await firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+      final user = AppUser(
+        uid: userCredential.user!.uid,
+        name: '',
+        email: email,
+      );
+      return user;
+    } catch (e) {
+      throw Exception('Login failed $e');
+    }
+  }
+
+  @override
+  Future<void> logOut() async {
+    await firebaseAuth.signOut();
+  }
+
+  @override
+  Future<AppUser?> getCurrentUser() async {
+    final firebaseUser = firebaseAuth.currentUser;
+    if (firebaseUser == null) {
+      return null;
+    }
+    return AppUser(uid: firebaseUser.uid, name: '', email: firebaseUser.email!);
   }
 }
