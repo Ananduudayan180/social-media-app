587726071be8e9ba1968a4bb7cc12a50616cb066
Anandu Udayan <anandhuudayan180@gamil.com>
2026-01-23 15:39:27 +0530

    feat: implement auth page switch logic

---
+++ lib/features/auth/presentation/pages/auth_pages.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:social_media_app/features/auth/presentation/pages/login_page.dart';
+import 'package:social_media_app/features/auth/presentation/pages/register_page.dart';
+
+class AuthPages extends StatefulWidget {
+  const AuthPages({super.key});
+
+  @override
+  State<AuthPages> createState() => _AuthPagesState();
+}
+
+class _AuthPagesState extends State<AuthPages> {
+  bool showLoginPage = true;
+
+  void togglePages() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginPage(onTap: togglePages);
+    } else {
+      return RegisterPage(toggle: togglePages);
+    }
+  }
+}
