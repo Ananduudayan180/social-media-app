f2c3efdc1b82f080dc1b55cf46cb5feb46d34e57
Anandu Udayan <anandhuudayan180@gamil.com>
2026-01-26 11:55:30 +0530

    feat: add my_drawer in homepage

---
+++ lib/features/post/presentation/pages/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_media_app/features/auth/presentation/cubit/auth_cubit.dart';
+import 'package:social_media_app/features/post/presentation/components/my_drawer.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.read<AuthCubit>().logout();
+            },
+            icon: Icon(Icons.logout),
+          ),
+        ],
+      ),
+      drawer: MyDrawer(),
+    );
+  }
+}
