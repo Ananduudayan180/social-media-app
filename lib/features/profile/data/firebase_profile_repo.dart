26cdd861ee7391c0e7633a83be5bea7f6eb36b1a
Anandu Udayan <anandhuudayan180@gamil.com>
2026-01-27 15:29:28 +0530

    feat: implement FirebaseProfileRepo with fetchUser and updateProfile

---
+++ lib/features/profile/data/firebase_profile_repo.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:social_media_app/features/profile/domain/entities/profile_user.dart';
+import 'package:social_media_app/features/profile/domain/repos/profile_repo.dart';
+
+class FirebaseProfileRepo extends ProfileRepo {
+  final CollectionReference firestore = FirebaseFirestore.instance.collection(
+    'user',
+  );
+
+  @override
+  Future<ProfileUser?> fetchUser(String uid) async {
+    try {
+      final userDoc = await firestore.doc(uid).get();
+      if (userDoc.exists) {
+        final userData = userDoc.data() as Map<String, dynamic>?;
+
+        if (userData != null) {
+          return ProfileUser(
+            email: userData['email'],
+            name: userData['name'],
+            uid: uid,
+            bio: userData['bio'],
+            profileImageUrl: userData['profileImageUrl'],
+          );
+        }
+      }
+      return null;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  @override
+  Future<void> updateProfile(ProfileUser updatedProfile) async {
+    try {
+      await firestore.doc(updatedProfile.uid).update({
+        'bio': updatedProfile.bio,
+        'profileImageUrl': updatedProfile.profileImageUrl,
+      });
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
