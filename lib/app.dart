6e90a4b86ace4b4a7b3c735f39268e014bc1aeaa
Anandu Udayan <anandhuudayan180@gamil.com>
2026-01-28 10:14:00 +0530

    refactor: replace BlocProvider with MultiBlocProvider

--- lib/app.dart
+++ lib/app.dart
@@ -6,6 +6,8 @@ import 'package:social_media_app/features/auth/presentation/cubit/auth_cubit.dar
 import 'package:social_media_app/features/auth/presentation/cubit/auth_state.dart';
 import 'package:social_media_app/features/auth/presentation/pages/auth_pages.dart';
 import 'package:social_media_app/features/home/presentation/pages/home_page.dart';
+import 'package:social_media_app/features/profile/data/firebase_profile_repo.dart';
+import 'package:social_media_app/features/profile/presentation/cubit/profile_cubit.dart';
 import 'package:social_media_app/themes/light_mode.dart';
 
 //root level of the app
@@ -13,10 +15,20 @@ class MyApp extends StatelessWidget {
   MyApp({super.key});
 
   final AuthRepo authRepo = FirebaseAuthRepo();
+  final profileRepo = FirebaseProfileRepo();
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AuthCubit(authRepo: authRepo)..checkAuth(),
+    return MultiBlocProvider(
+      providers: [
+        //Auth cubit
+        BlocProvider<AuthCubit>(
+          create: (context) => AuthCubit(authRepo: authRepo)..checkAuth(),
+        ),
+        //Profile Cubit
+        BlocProvider<ProfileCubit>(
+          create: (context) => ProfileCubit(profileRepo: profileRepo),
+        ),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: lightMode,
