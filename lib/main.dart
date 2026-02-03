342defef3c0738f86c87f0b963b19384945070ee
Anandu Udayan <anandhuudayan180@gamil.com>
2026-02-03 21:39:30 +0530

    chore: initialize supabase

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,17 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:social_media_app/app.dart';
 import 'package:social_media_app/config/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  //supabase
+  await Supabase.initialize(
+    url: 'https://apdsanolnviefpxojezz.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwZHNhbm9sbnZpZWZweG9qZXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMjA5NDIsImV4cCI6MjA4NTY5Njk0Mn0.SRHBBIpFsDPicNqpWp6rEtmBQsNPwK3OYut4cb-wFIk',
+  );
   runApp(MyApp());
 }
