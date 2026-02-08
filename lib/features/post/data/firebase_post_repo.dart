d1c7db553d31b2d35397394ba8183f31e1a223a2
Anandu Udayan <anandhuudayan180@gamil.com>
2026-02-08 15:32:44 +0530

    feat: add firebase post repositry crud operations

---
+++ lib/features/post/data/firebase_post_repo.dart
@@ -0,0 +1,65 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:social_media_app/features/post/domain/entities/post.dart';
+import 'package:social_media_app/features/post/domain/repos/post_repo.dart';
+
+class FirebasePostRepo extends PostRepo {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final CollectionReference postsCollection = FirebaseFirestore.instance
+      .collection('posts');
+
+  @override
+  Future<void> createPost(Post post) async {
+    try {
+      await postsCollection.doc(post.id).set({post.toJson()});
+    } catch (e) {
+      throw Exception('Error creating post: $e');
+    }
+  }
+
+  @override
+  Future<void> deletePost(String postId) async {
+    try {
+      await postsCollection.doc(postId).delete();
+    } catch (e) {
+      throw Exception('Error deleting post: $e');
+    }
+  }
+
+  @override
+  Future<List<Post>> fetchAllPosts() async {
+    try {
+      //get all posts with most recent post at the top
+      final postsSnapshot = await postsCollection
+          .orderBy('timestamp', descending: true)
+          .get();
+
+      //convert each firestore document from json -> list of posts
+      final List<Post> allPost = postsSnapshot.docs
+          .map((doc) => Post.fromJson(doc.data() as Map<String, dynamic>))
+          .toList();
+
+      return allPost;
+    } catch (e) {
+      throw Exception('Error fetching posts: $e');
+    }
+  }
+
+  @override
+  Future<List<Post>> fetchPostByUserId(String userId) async {
+    try {
+      //fetch posts snapshot with this uid
+      final postSnapshot = await postsCollection
+          .where('userId', isEqualTo: userId)
+          .get();
+
+      // convert firestore documents from json -> list of posts
+      final userPosts = postSnapshot.docs
+          .map((doc) => Post.fromJson(doc.data() as Map<String, dynamic>))
+          .toList();
+
+      return userPosts;
+    } catch (e) {
+      throw Exception('Error fetching posts by user: $e');
+    }
+  }
+}
