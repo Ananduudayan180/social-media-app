388ea7bcb1220966269b3931959e0a5081708bb9
Anandu Udayan <anandhuudayan180@gamil.com>
2026-02-09 09:44:42 +0530

    feat: add upload post page with pickImage and uploadPost functions

---
+++ lib/features/post/presentation/pages/upload_post_page.dart
@@ -0,0 +1,145 @@
+import 'dart:io';
+import 'dart:math';
+import 'dart:typed_data';
+import 'package:flutter/foundation.dart' show kIsWeb; //true == web
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_media_app/features/auth/domain/entities/app_user.dart';
+import 'package:social_media_app/features/auth/presentation/cubit/auth_cubit.dart';
+import 'package:social_media_app/features/post/domain/entities/post.dart';
+import 'package:social_media_app/features/post/presentation/cubit/post_cubit.dart';
+import 'package:social_media_app/features/post/presentation/cubit/post_state.dart';
+
+class UploadPostPage extends StatefulWidget {
+  const UploadPostPage({super.key});
+
+  @override
+  State<UploadPostPage> createState() => _UploadPostPagesState();
+}
+
+class _UploadPostPagesState extends State<UploadPostPage> {
+  //
+  PlatformFile? imagePickedFile;
+  Uint8List? webImage;
+  final textController = TextEditingController();
+  AppUser? currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    final authCubit = context.read<AuthCubit>();
+    currentUser = authCubit.currentUser;
+  }
+
+  //pick image function
+  Future<void> pickImage() async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      withData: kIsWeb,
+    );
+
+    if (result != null) {
+      setState(() {
+        imagePickedFile = result.files.first;
+      });
+
+      if (kIsWeb) {
+        webImage = imagePickedFile!.bytes;
+      }
+    }
+  }
+
+  //create & upload
+  void uploadPost() {
+    //check if both text and image are empty
+    if (imagePickedFile == null || textController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Please add image and caption")),
+      );
+      return;
+    }
+    //create new post object
+    final newPost = Post(
+      id: DateTime.now().millisecondsSinceEpoch.toString(),
+      userId: currentUser!.uid,
+      userName: currentUser!.name,
+      text: textController.text,
+      imageUrl: '',
+      timestamp: DateTime.now(),
+    );
+
+    final postCubit = context.read<PostCubit>();
+
+    //web upload
+    if (kIsWeb) {
+      postCubit.createPost(newPost, imageBytes: imagePickedFile?.bytes);
+    } else {
+      //mobile upload
+      postCubit.createPost(newPost, imagePath: imagePickedFile?.path);
+    }
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  //UI
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<PostCubit, PostState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        if (state is PostUploading || state is PostsLoading) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+        return buildUploadPage();
+      },
+    );
+  }
+
+  Widget buildUploadPage() {
+    //Scafold
+    return Scaffold(
+      //AppBar
+      appBar: AppBar(
+        title: const Text('Create Post'),
+        centerTitle: true,
+        foregroundColor: Theme.of(context).colorScheme.primary,
+        actions: [IconButton(onPressed: uploadPost, icon: Icon(Icons.check))],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            //Image preview
+            if (kIsWeb && webImage != null)
+              Image.memory(webImage!, fit: BoxFit.cover),
+            if (!kIsWeb && imagePickedFile != null)
+              Image.file(File(imagePickedFile!.path!), fit: BoxFit.cover),
+            //pick image button
+            MaterialButton(
+              onPressed: pickImage,
+              color: Colors.blue,
+              child: Text('Pick Image'),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: textController,
+              decoration: InputDecoration(hintText: 'Write a caption...'),
+              maxLines: null,
+              obscureText: false,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
