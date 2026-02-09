d4a711e876273accba745ee82dfbd0f0bcc76223
Anandu Udayan <anandhuudayan180@gamil.com>
2026-02-09 16:48:28 +0530

    feat: build posts screen using BlocBuilder

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,7 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:social_media_app/features/auth/presentation/cubit/auth_cubit.dart';
 import 'package:social_media_app/features/home/presentation/components/my_drawer.dart';
+import 'package:social_media_app/features/post/presentation/cubit/post_cubit.dart';
+import 'package:social_media_app/features/post/presentation/cubit/post_state.dart';
+import 'package:social_media_app/features/post/presentation/pages/upload_post_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -45,6 +48,47 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       drawer: MyDrawer(),
+      body: BlocBuilder<PostCubit, PostState>(
+        builder: (context, state) {
+          //loading
+          if (state is PostsLoading || state is PostUploading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          //loaded
+          else if (state is PostsLoaded) {
+            final allPosts = state.posts;
+            //empty list
+            if (allPosts.isEmpty) {
+              return const Center(child: Text('No posts available'));
+            }
+            //post list builder
+            return ListView.builder(
+              itemCount: allPosts.length,
+              itemBuilder: (context, index) {
+                final post = allPosts[index];
+                //post card
+                return CachedNetworkImage(
+                  imageUrl: post.imageUrl,
+                  height: 430,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                  //placeholder
+                  placeholder: (context, url) => const SizedBox(
+                    height: 430,
+                    child: Center(child: CircularProgressIndicator()),
+                  ),
+                  //error widget
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                );
+              },
+            );
+          } else if (state is PostError) {
+            return Center(child: Text(state.errorMsg));
+          } else {
+            return const SizedBox();
+          }
+        },
+      ),
     );
   }
 }
