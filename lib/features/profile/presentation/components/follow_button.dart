3b5d584ea67d6229640390e44d8983144d8ce5b3
Anandu Udayan <anandhuudayan180@gamil.com>
2026-02-12 17:40:54 +0530

    feat: add FollowButton widget for follow/unfollow users

---
+++ lib/features/profile/presentation/components/follow_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class FollowButton extends StatelessWidget {
+  final void Function()? onPressed;
+  final bool isFollowing;
+  const FollowButton({
+    super.key,
+    required this.onPressed,
+    required this.isFollowing,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        //Follow Button
+        child: MaterialButton(
+          onPressed: onPressed,
+          padding: const EdgeInsets.all(25),
+          color: isFollowing
+              ? Theme.of(context).colorScheme.primary
+              : Colors.blue,
+          child: Text(
+            isFollowing ? 'Unfollow' : 'Follow',
+            //style
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.secondary,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
