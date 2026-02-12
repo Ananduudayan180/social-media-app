bde270b06e15c06d1705b6c608455d5a36c9c86a
Anandu Udayan <anandhuudayan180@gamil.com>
2026-02-12 12:47:33 +0530

    feat: update ProfileUser model to include followers and following

--- lib/features/profile/domain/entities/profile_user.dart
+++ lib/features/profile/domain/entities/profile_user.dart
@@ -3,6 +3,8 @@ import 'package:social_media_app/features/auth/domain/entities/app_user.dart';
 class ProfileUser extends AppUser {
   final String bio;
   final String profileImageUrl;
+  final List<String> followers;
+  final List<String> following;
 
   ProfileUser({
     required super.email,
@@ -10,28 +12,39 @@ class ProfileUser extends AppUser {
     required super.uid,
     required this.bio,
     required this.profileImageUrl,
+    required this.followers,
+    required this.following,
   });
 
   //method to update profile user
 
-  ProfileUser copyWith({String? newBio, String? newProfileImageUrl}) {
+  ProfileUser copyWith({
+    String? newBio,
+    String? newProfileImageUrl,
+    List<String>? newfollowers,
+    List<String>? newfollowing,
+  }) {
     return ProfileUser(
       email: email,
       name: name,
       uid: uid,
       bio: newBio ?? bio,
       profileImageUrl: newProfileImageUrl ?? profileImageUrl,
+      followers: newfollowers ?? followers,
+      following: newfollowing ?? following,
     );
   }
 
   @override
-  Map<String, String> toJson() {
+  Map<String, dynamic> toJson() {
     return {
       'uid': uid,
       'name': name,
       'email': email,
       'bio': bio,
       'profileImageUrl': profileImageUrl,
+      'followers': followers,
+      'following': following,
     };
   }
 
@@ -42,6 +55,8 @@ class ProfileUser extends AppUser {
       uid: json['uid'],
       bio: json['bio'] ?? '',
       profileImageUrl: json['profileImageUrl'] ?? '',
+      followers: List<String>.from(json['followers'] ?? []),
+      following: List<String>.from(json['following'] ?? []),
     );
   }
 }
