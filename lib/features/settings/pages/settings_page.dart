f22f1477fe85106e0ec2c60d3bbf7de27b0b2305
Anandu Udayan <anandhuudayan180@gamil.com>
2026-02-14 13:10:51 +0530

    feat: add SettingsPage to toggle dark and light theme

---
+++ lib/features/settings/pages/settings_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_media_app/themes/theme_cubit.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeCubit = context.watch<ThemeCubit>();
+
+    //is dark mode
+    bool isDarkMode = themeCubit.isDarkMode;
+    return Scaffold(
+      appBar: AppBar(title: Text('Settings')),
+      body: Column(
+        children: [
+          ListTile(
+            title: Text('Dark Mode'),
+            trailing: CupertinoSwitch(
+              value: isDarkMode,
+              onChanged: (value) {
+                themeCubit.toggleTheme();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
