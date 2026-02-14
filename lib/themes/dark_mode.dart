13d424a162d567c7eae40a099c67a2f95e4ba43d
Anandu Udayan <anandhuudayan180@gamil.com>
2026-02-14 13:08:06 +0530

    feat: define dark mode ThemeData

---
+++ lib/themes/dark_mode.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+  colorScheme: const ColorScheme.dark(
+    //very dart
+    surface: Color.fromARGB(255, 9, 9, 9),
+    //slightly light
+    primary: Color.fromARGB(255, 105, 105, 105),
+    //dark
+    secondary: Color.fromARGB(255, 20, 20, 20),
+    //slightly dark
+    inversePrimary: Color.fromARGB(255, 195, 195, 195),
+  ),
+  scaffoldBackgroundColor: Color.fromARGB(255, 9, 9, 9),
+);
